6a5ac5d65349994b2db8f6a705ce7963e32c9bd5
Manuel <manuel03ga11@hotmail.com>
2020-10-04 21:35:49 -0300

    Ejercicio 1

--- factorial_iterativo.asm
+++ factorial_iterativo.asm
@@ -11,6 +11,13 @@ li $a3, 1 #f = 1
 
 jal fact # llama a fact
 
+print:
+# Imprime F
+move $a0, $a3 # Guarda a3 en a0 para imprimir F
+li $v0, 1 # Carga 1 en v0 (instruccion para imprimir un int)
+syscall # Imprime F
+move $v0, $a0 # Devuelve v0
+
 exit: # Sale del programa
 li $v0, 10
 syscall
@@ -25,12 +32,5 @@ mflo  $a3   # lo guarda en v0
 
 addi  $a0, $a0, 1  # Suma 1 a C cada vez que se itera el ciclo
 j loop #reitera el ciclo
-
-print:
-# Imprime F
-move $a0, $a3 # Guarda a3 en a0 para imprimir F
-li $v0, 1 # Carga 1 en v0 (instruccion para imprimir un int)
-syscall # Imprime F
-move $v0, $a0 # Devuelve v0
 
 jr $ra # return F (v0)
