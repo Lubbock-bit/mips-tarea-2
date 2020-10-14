408eda02229349fa5424e9a740ccda274e8bcfa0
Manuel <manuel03ga11@hotmail.com>
2020-10-14 19:52:21 -0300

    Ejercicio 2

---
+++ Factorial_recursivo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdio.conio>
+
+int main(){
+  n=
+  fact_recursivo(n);
+
+  return 0;
+}
+
+int fact_recursivo(n){
+  // caso base de factoreo
+   if(n==0){
+     printf("%d\n", n);
+  }
+   if(n==1){
+     printf("%d\n", n);
+   }
+return
+}
